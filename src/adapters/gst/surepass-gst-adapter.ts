6b2220fdba4d0f99001923cdf07e223569a7f6fd
Mohit Khatri <mohit4bug@gmail.com>
2025-03-25 11:45:50 +0530

    refactor: different response

--- src/adapters/gst/surepass-gst-adapter.ts
+++ src/adapters/gst/surepass-gst-adapter.ts
@@ -21,7 +21,7 @@ export class SurepassGstAdapter implements BaseGstAdapter {
 
   verify = async (params: IVerifyParams): Promise<IVerifyResponse> => {
     try {
-      const res = await this.axiosClient.post('/corporate-otp/gstin/init', {
+      const res = await this.axiosClient.post('/corporate/gstin', {
         id_number: params.gstNumber
       })
 
@@ -29,160 +29,66 @@ export class SurepassGstAdapter implements BaseGstAdapter {
        * @example
        * {
        *   "data": {
-       *     "client_id": "gst_otp_fedWdafvBBpblZTofJaw",
-       *     "email": "RIL.MHG@ril.com",
-       *     "mobile": "9900012345",
-       *     "fallback": false,
-       *     "details": {
-       *       "contact_details": {
-       *         "principal": {
-       *           "address": "Thane Belapur Road, Ghansoli, Navi Mumbai, Thane, Maharashtra, 400701",
-       *           "email": "RIL.MHG@ril.com",
-       *           "mobile": "9900012345",
-       *           "nature_of_business": "Factory / Manufacturing, Retail Business, Input Service Distributor (ISD), Bonded Warehouse, SEZ, Works Contract, Warehouse / Depot, Service Provision, Leasing Business, Office / Sale Office, Recipient of Goods or Services, Wholesale Business, EOU / STP / EHTP"
-       *         },
-       *         "additional": [
-       *           {
-       *             "address": "IV, MAKER CHAMBERS, NARIMAN POINT, MUMBAI, Mumbai, Maharashtra, 400021",
-       *             "email": "RIL.MHG@ril.com",
-       *             "mobile": "9900012345",
-       *             "nature_of_business": "Factory / Manufacturing, Retail Business, Input Service Distributor (ISD), Bonded Warehouse, SEZ, Works Contract, Warehouse / Depot, Service Provision, Leasing Business, Office / Sale Office, Recipient of Goods or Services, Wholesale Business, EOU / STP / EHTP"
-       *           },
-       *           {
-       *             "address": "CHEMICAL TERMINAL BOMBAY LTD, PIRPAU INSTALLATION, NEAR BPT PUMP HOUSE, TROMBE, Mumbai, Maharashtra, 400074",
-       *             "email": "RIL.MHG@ril.com",
-       *             "mobile": "9900012345",
-       *             "nature_of_business": "Factory / Manufacturing, Retail Business, Input Service Distributor (ISD), Bonded Warehouse, SEZ, Works Contract, Warehouse / Depot, Service Provision, Leasing Business, Office / Sale Office, Recipient of Goods or Services, Wholesale Business, EOU / STP / EHTP"
-       *           }
-       *         ]
-       *       },
-       *       "promoters": [
-       *         "Nikhil Rasiklal Meswani ",
-       *         "Hital Rasiklal Meswani ",
-       *         "Mukesh Dhirubhai Ambani ",
-       *         "Madhusudana Sivaprasad Panda "
-       *       ],
-       *       "annual_turnover": "Slab: Rs. 500 Cr. and above",
-       *       "annual_turnover_fy": "2022-2023",
-       *       "percentage_in_cash_fy": "",
-       *       "percentage_in_cash": "NA",
-       *       "aadhaar_validation": "Yes",
-       *       "aadhaar_validation_date": "2024-02-28",
-       *       "address_details": {},
-       *       "liability_percentage_details": {},
-       *       "less_info": false,
-       *       "client_id": null,
-       *       "gstin": "27AAACR1234K1Z7",
-       *       "pan_number": "AAACR1234K",
-       *       "business_name": "RELIANCE INDUSTRIES LIMITED",
-       *       "legal_name": "RELIANCE INDUSTRIES LIMITED",
-       *       "center_jurisdiction": "Commissionerate - BELAPUR,Division - DIVISION IV,Range - RANGE-IV (Jurisdictional Office)",
-       *       "state_jurisdiction": "State - Maharashtra,Zone - Thane,Division - RAIGAD,Charge - URAN_701",
-       *       "date_of_registration": "2017-07-01",
-       *       "constitution_of_business": "Public Limited Company",
-       *       "taxpayer_type": "Regular",
-       *       "gstin_status": "Active",
-       *       "date_of_cancellation": "1800-01-01",
-       *       "field_visit_conducted": "No",
-       *       "nature_bus_activities": [
-       *         "Factory / Manufacturing",
-       *         "Retail Business",
-       *         "Input Service Distributor (ISD)",
-       *         "Bonded Warehouse",
-       *         "SEZ",
-       *         "Works Contract",
-       *         "Warehouse / Depot",
-       *         "Service Provision",
-       *         "Leasing Business",
-       *         "Office / Sale Office",
-       *         "Recipient of Goods or Services",
-       *         "Wholesale Business",
-       *         "EOU / STP / EHTP",
-       *         "Supplier of Services",
-       *         "Others",
-       *         "Export"
-       *       ],
-       *       "nature_of_core_business_activity_code": "MFT",
-       *       "nature_of_core_business_activity_description": "Manufacturer",
-       *       "filing_status": [],
-       *       "address": null,
-       *       "hsn_info": {},
-       *       "filing_frequency": []
-       *     }
+       *     "address_details": {},
+       *     "client_id": "corporate_gstin_hemuprVyNGvtQAvRJHXy",
+       *     "gstin": "08AKWPJ1234H1ZN",
+       *     "pan_number": "AKWPJ1234H",
+       *     "business_name": "MINDA MARWAR PRODUCER COMPANY",
+       *     "legal_name": "MADAN LAL JAT",
+       *     "center_jurisdiction": "Commissionerate - JODHPUR,Division - GST DIVISION",
+       *     "state_jurisdiction": "State - Rajasthan,Zone",
+       *     "date_of_registration": "2021-10-20",
+       *     "constitution_of_business": "Proprietorship",
+       *     "taxpayer_type": "Regular",
+       *     "gstin_status": "Active",
+       *     "date_of_cancellation": "1800-01-01",
+       *     "field_visit_conducted": "No",
+       *     "nature_bus_activities": [
+       *       "Retail Business",
+       *       "Wholesale Business"
+       *     ],
+       *     "nature_of_core_business_activity_code": "NA",
+       *     "nature_of_core_business_activity_description": "NA",
+       *     "aadhaar_validation": "Yes",
+       *     "aadhaar_validation_date": "2021-10-20",
+       *     "filing_status": [],
+       *     "address": "MINDA NAVA, WARD NO. 15, VILL. BHEEVPURA",
+       *     "hsn_info": {},
+       *     "filing_frequency": []
        *   },
        *   "status_code": 200,
        *   "success": true,
-       *   "message": "Success",
+       *   "message": null,
        *   "message_code": "success"
        * }
        */
       return {
         data: {
+          addressDetails: res.data?.data?.address_details || {},
           clientId: res.data?.data?.client_id,
-          email: res.data?.data?.email,
-          mobile: res.data?.data?.mobile,
-          fallback: res.data?.data?.fallback,
-          details: {
-            contactDetails: {
-              principal: {
-                address:
-                  res.data?.data?.details?.contact_details?.principal?.address,
-                email:
-                  res.data?.data?.details?.contact_details?.principal?.email,
-                mobile:
-                  res.data?.data?.details?.contact_details?.principal?.mobile,
-                natureOfBusiness:
-                  res.data?.data?.details?.contact_details?.principal
-                    ?.nature_of_business
-              },
-              additional:
-                res.data?.data?.details?.contact_details?.additional?.map(
-                  (item: any) => ({
-                    address: item?.address,
-                    email: item?.email,
-                    mobile: item?.mobile,
-                    natureOfBusiness: item?.nature_of_business
-                  })
-                ) || []
-            },
-            promoters: res.data?.data?.details?.promoters || [],
-            annualTurnover: res.data?.data?.details?.annual_turnover,
-            annualTurnoverFy: res.data?.data?.details?.annual_turnover_fy,
-            percentageInCashFy: res.data?.data?.details?.percentage_in_cash_fy,
-            percentageInCash: res.data?.data?.details?.percentage_in_cash,
-            aadhaarValidation: res.data?.data?.details?.aadhaar_validation,
-            aadhaarValidationDate:
-              res.data?.data?.details?.aadhaar_validation_date,
-            addressDetails: res.data?.data?.details?.address_details || {},
-            liabilityPercentageDetails:
-              res.data?.data?.details?.liability_percentage_details || {},
-            lessInfo: res.data?.data?.details?.less_info,
-            clientId: res.data?.data?.details?.client_id,
-            gstin: res.data?.data?.details?.gstin,
-            panNumber: res.data?.data?.details?.pan_number,
-            businessName: res.data?.data?.details?.business_name,
-            legalName: res.data?.data?.details?.legal_name,
-            centerJurisdiction: res.data?.data?.details?.center_jurisdiction,
-            stateJurisdiction: res.data?.data?.details?.state_jurisdiction,
-            dateOfRegistration: res.data?.data?.details?.date_of_registration,
-            constitutionOfBusiness:
-              res.data?.data?.details?.constitution_of_business,
-            taxpayerType: res.data?.data?.details?.taxpayer_type,
-            gstinStatus: res.data?.data?.details?.gstin_status,
-            dateOfCancellation: res.data?.data?.details?.date_of_cancellation,
-            fieldVisitConducted: res.data?.data?.details?.field_visit_conducted,
-            natureBusActivities:
-              res.data?.data?.details?.nature_bus_activities || [],
-            natureOfCoreBusinessActivityCode:
-              res.data?.data?.details?.nature_of_core_business_activity_code,
-            natureOfCoreBusinessActivityDescription:
-              res.data?.data?.details
-                ?.nature_of_core_business_activity_description,
-            filingStatus: res.data?.data?.details?.filing_status || [],
-            address: res.data?.data?.details?.address,
-            hsnInfo: res.data?.data?.details?.hsn_info || {},
-            filingFrequency: res.data?.data?.details?.filing_frequency || []
-          }
+          gstin: res.data?.data?.gstin,
+          panNumber: res.data?.data?.pan_number,
+          businessName: res.data?.data?.business_name,
+          legalName: res.data?.data?.legal_name,
+          centerJurisdiction: res.data?.data?.center_jurisdiction,
+          stateJurisdiction: res.data?.data?.state_jurisdiction,
+          dateOfRegistration: res.data?.data?.date_of_registration,
+          constitutionOfBusiness: res.data?.data?.constitution_of_business,
+          taxpayerType: res.data?.data?.taxpayer_type,
+          gstinStatus: res.data?.data?.gstin_status,
+          dateOfCancellation: res.data?.data?.date_of_cancellation,
+          fieldVisitConducted: res.data?.data?.field_visit_conducted,
+          natureBusActivities: res.data?.data?.nature_bus_activities || [],
+          natureOfCoreBusinessActivityCode:
+            res.data?.data?.nature_of_core_business_activity_code,
+          natureOfCoreBusinessActivityDescription:
+            res.data?.data?.nature_of_core_business_activity_description,
+          aadhaarValidation: res.data?.data?.aadhaar_validation,
+          aadhaarValidationDate: res.data?.data?.aadhaar_validation_date,
+          filingStatus: res.data?.data?.filing_status || [],
+          address: res.data?.data?.address,
+          hsnInfo: res.data?.data?.hsn_info || {},
+          filingFrequency: res.data?.data?.filing_frequency || []
         }
       }
     } catch (error) {
--- src/adapters/gst/types.ts
+++ src/adapters/gst/types.ts
@@ -7,55 +7,28 @@ export interface IVerifyParams {
 
 export interface IVerifyResponse {
   data: {
+    addressDetails: Record<string, unknown>
     clientId: string
-    email: string
-    mobile: string
-    fallback: boolean
-    details: {
-      contactDetails: {
-        principal: {
-          address: string
-          email: string
-          mobile: string
-          natureOfBusiness: string
-        }
-        additional: {
-          address: string
-          email: string
-          mobile: string
-          natureOfBusiness: string
-        }[]
-      }
-      promoters: string[]
-      annualTurnover: string
-      annualTurnoverFy: string
-      percentageInCashFy: string
-      percentageInCash: string
-      aadhaarValidation: string
-      aadhaarValidationDate: string
-      addressDetails: Record<string, unknown>
-      liabilityPercentageDetails: Record<string, unknown>
-      lessInfo: boolean
-      clientId: string | null
-      gstin: string
-      panNumber: string
-      businessName: string
-      legalName: string
-      centerJurisdiction: string
-      stateJurisdiction: string
-      dateOfRegistration: string
-      constitutionOfBusiness: string
-      taxpayerType: string
-      gstinStatus: string
-      dateOfCancellation: string
-      fieldVisitConducted: string
-      natureBusActivities: string[]
-      natureOfCoreBusinessActivityCode: string
-      natureOfCoreBusinessActivityDescription: string
-      filingStatus: unknown[]
-      address: string | null
-      hsnInfo: Record<string, unknown>
-      filingFrequency: unknown[]
-    }
+    gstin: string
+    panNumber: string
+    businessName: string
+    legalName: string
+    centerJurisdiction: string
+    stateJurisdiction: string
+    dateOfRegistration: string
+    constitutionOfBusiness: string
+    taxpayerType: string
+    gstinStatus: string
+    dateOfCancellation: string
+    fieldVisitConducted: string
+    natureBusActivities: string[]
+    natureOfCoreBusinessActivityCode: string
+    natureOfCoreBusinessActivityDescription: string
+    aadhaarValidation: string
+    aadhaarValidationDate: string
+    filingStatus: unknown[]
+    address: string
+    hsnInfo: Record<string, unknown>
+    filingFrequency: unknown[]
   }
 }
